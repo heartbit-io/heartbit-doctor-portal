f558349b884bee9bf84498f9d6d6542dfcf6b412
Nodirbek Jamoldinov <nick@washswat.com>
2023-05-10 23:03:08 +0500

    project set up and add top bar

---
+++ src/components/surface/TopBar.tsx
@@ -0,0 +1,106 @@
+"use client";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+
+const pages = ["Take Questions", "My Answers"];
+const settings = ["Sign-out"];
+
+function TopBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{ background: "#fff", padding: "0 40px" }}>
+      <Toolbar disableGutters>
+        <img src="img/logo.png" />
+
+        <Box
+          sx={{
+            flexGrow: 1,
+            justifyContent: "center",
+            display: { xs: "none", md: "flex" },
+          }}
+        >
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+              sx={{
+                my: 2,
+                color: "#3A3A3C",
+                display: "block",
+              }}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Sign-out">
+            <Button
+              sx={{
+                background: "white",
+                color: "#3A3A3C",
+                border: "1px solid #3A3A3C",
+                borderRadius: 14,
+              }}
+              size="small"
+              variant="outlined"
+              onClick={handleOpenUserMenu}
+            >
+              dr@hospital.com
+            </Button>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+export default TopBar;
