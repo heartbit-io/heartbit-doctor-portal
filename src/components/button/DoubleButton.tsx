7d5b7e095da8e8396aaa2f8c034604dea1527c41
Nodirbek Jamoldinov <nick@washswat.com>
2023-05-11 17:53:36 +0500

    update Doublebutton to get details as a prop

--- src/components/button/DoubleButton.tsx
+++ src/components/button/DoubleButton.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 import { Box, Button, Stack, Typography } from "@mui/material";
 
-const DoubleButton = () => {
+type Props = {
+  bounty?: string | number;
+  date?: string | number;
+  cancelBtn: {
+    text: string;
+    onClick: () => void;
+    style?: any;
+  };
+  confirmBtn: {
+    text: string;
+    onClick: () => void;
+    style?: any;
+  };
+};
+
+const DoubleButton = ({ bounty, date, cancelBtn, confirmBtn }: Props) => {
   return (
     <Box
       display={"flex"}
@@ -11,29 +26,39 @@ const DoubleButton = () => {
       mb={4}
     >
       <Stack spacing={5} direction="row">
-        <Stack spacing={1} direction="row">
-          <Typography sx={{ fontWeight: "bold" }}>Bounty:</Typography>
-          <Typography>1,000sats</Typography>
-        </Stack>
-        <Stack spacing={1} direction="row">
-          <Typography sx={{ fontWeight: "bold" }}>Date:</Typography>
-          <Typography>10:49AM, 9th Mar 2023</Typography>
-        </Stack>
+        {!!bounty && (
+          <Stack spacing={1} direction="row">
+            <Typography sx={{ fontWeight: "bold" }}>Bounty:</Typography>
+            <Typography>{bounty} sats</Typography>
+          </Stack>
+        )}
+        {!!date && (
+          <Stack spacing={1} direction="row">
+            <Typography sx={{ fontWeight: "bold" }}>Date:</Typography>
+            <Typography>{date}</Typography>
+          </Stack>
+        )}
       </Stack>
       <Stack spacing={1} direction="row">
         <Button
           variant="text"
           size="large"
-          sx={{ color: "#3A3A3C", padding: "8px 60px" }}
+          sx={{ color: "#3A3A3C", padding: "8px 60px", ...cancelBtn?.style }}
+          onClick={cancelBtn?.onClick}
         >
-          Pass
+          {cancelBtn?.text}
         </Button>
         <Button
           variant="contained"
           size="large"
-          style={{ backgroundColor: "#34C759", padding: "8px 60px" }}
+          style={{
+            backgroundColor: "#34C759",
+            padding: "8px 60px",
+            ...confirmBtn?.style,
+          }}
+          onClick={confirmBtn?.onClick}
         >
-          Take
+          {confirmBtn?.text}
         </Button>
       </Stack>
     </Box>
