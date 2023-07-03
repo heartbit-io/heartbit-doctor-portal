85c1fca814054450dab6180785a768b61d0d5ebd
Nodirbek Jamoldinov <nick@heartbit.io>
2023-07-03 16:48:43 +0500

    update email sent screen to match figma design

--- src/app/email-sent/page.tsx
+++ src/app/email-sent/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 
 const Page = () => {
   const [email, setEmail] = useState("");
@@ -15,9 +15,20 @@ const Page = () => {
       justifyContent={"center"}
       alignItems={"center"}
     >
-      <Box>
+      <Box
+        display="flex"
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
         <img src="img/logo_lg.svg" />
-        <Typography textAlign={"center"} variant="h5" mt={5} mb={3}>
+        <Typography
+          textAlign={"center"}
+          fontWeight={"bold"}
+          variant="h5"
+          mt={5}
+          mb={3}
+        >
           Check your email inbox
         </Typography>
         <Typography textAlign={"center"} variant="body1">
@@ -26,6 +37,22 @@ const Page = () => {
         <Typography textAlign={"center"} variant="body1" fontWeight={"bold"}>
           {email}
         </Typography>
+        <Typography
+          textAlign={"center"}
+          variant="body1"
+          color={"#3A3A3C"}
+          mt={5}
+        >
+          Didn’t get your email or something not working?
+        </Typography>
+        <Link
+          style={{ textDecoration: "none" }}
+          variant="body1"
+          color={"#FF2D55"}
+          href="/sign-in"
+        >
+          Try again
+        </Link>
       </Box>
     </Box>
   );
