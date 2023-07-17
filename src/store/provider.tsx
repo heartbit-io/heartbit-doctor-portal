9445ee06348d8a8f722ba5e33392f70e5de41323
Nodirbek Jamoldinov <nick@heartbit.io>
2023-07-17 17:35:42 +0500

    intercom added

--- src/store/provider.tsx
+++ src/store/provider.tsx
@@ -4,6 +4,7 @@ import { store } from "./";
 import { Provider } from "react-redux";
 import { auth } from "`@/firebase`";
 import { api } from "`@/apis`";
+import { loadIntercom } from "next-intercom";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   useEffect(() => {
@@ -22,6 +23,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
         if (token) {
           localStorage.setItem("token", token);
           api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+          loadIntercom({
+            appId: "v55v85ev",
+            name: user.email,
+          });
         }
       }
     });
