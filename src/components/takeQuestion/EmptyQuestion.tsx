207e2d8ddc191d8e8702f11e38e0e398c9af28c0
Nodirbek Jamoldinov <nick@heartbit.io>
2023-08-28 12:43:37 +0500

    added join to discord button in EmptyQuestion and wording changes

--- src/components/takeQuestion/EmptyQuestion.tsx
+++ src/components/takeQuestion/EmptyQuestion.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import React from "react";
 
 const EmptyQuestion = () => {
@@ -17,10 +17,27 @@ const EmptyQuestion = () => {
         textAlign={"center"}
         mt={3}
       >
-        {
-          "All patients' questions were answered. \nThank you for your service, and please come back again."
-        }
+        You have answered all the questions.
+        <br />
+        While you wait for new questions, why not take a look at the HeartBit
+        community?
       </Typography>
+      <Button
+        variant="contained"
+        onClick={() => window.open("https://discord.gg/Gq6Xerb7sa")}
+        startIcon={<img src="img/discord.svg" style={{ color: "white" }} />}
+        style={{
+          height: 48,
+          borderRadius: 10,
+          backgroundColor: "#5A65EA",
+          fontSize: 17,
+          fontWeight: 600,
+          textTransform: "capitalize",
+          marginTop: 32,
+        }}
+      >
+        Join Community
+      </Button>
     </Box>
   );
 };
