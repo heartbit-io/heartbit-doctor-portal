918b42d817f3f7c3a80ce5c25534e68efd98a1e4
Nodirbek Jamoldinov <nick@heartbit.io>
2023-09-01 14:41:09 +0500

    added icon in the header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Providers } from "./provider";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="shortcut icon" href="img/logo.png" />
+      </Head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
