e76a3c8efcf63ee8938f57011122a6e65d43f507
Nodirbek Jamoldinov <nick@heartbit.io>
2023-09-04 19:02:21 +0500

    update styling in take-question page from mui to styled-components

--- src/app/take-question/page.tsx
+++ src/app/take-question/page.tsx
@@ -1,19 +1,24 @@
 "use client";
 
-import { Box, Container, Divider, Fade, Typography } from "@mui/material";
-import { DoubleButton, NavBar, Loading, EmptyQuestion } from "`@/components`";
 import { useEffect, useState } from "react";
-
-import { assignQuestion, getQuestion } from "`@/apis/questionApi`";
+import { Divider } from "@mui/material";
 import { useRouter } from "next/navigation";
-import { getBtcRates } from "`@/apis/coinApi`";
 import { useAppSelector } from "`@/hooks/hooks`";
+import { styled } from "styled-components";
+
+// components
+import { DoubleButton, NavBar, EmptyQuestion, Loading } from "`@/components`";
+
+// apis
+import { assignQuestion, getQuestion } from "`@/apis/questionApi`";
+import { getBtcRates } from "`@/apis/coinApi`";
 
 export default function Page() {
   const router = useRouter();
   const { userData } = useAppSelector((state) => state.user);
   const [question, setQuestion] = useState<any>();
   const [loading, setLoading] = useState(true);
+  const [showSpinner, setShowSpinner] = useState(false);
   const [questionIndex, setQuestionIndex] = useState(0);
   const [USDPerSat, setUSDPerSat] = useState(0);
 
@@ -30,8 +35,9 @@ export default function Page() {
     return () => clearInterval(interval);
   }, []);
 
-  const fetchQuestion = async () => {
+  const fetchQuestion = async (showSpinner = false) => {
     setLoading(true);
+    setShowSpinner(showSpinner);
     getQuestion(questionIndex)
       .then((res) => {
         if (res?.success && res?.statusCode === 200) {
@@ -45,10 +51,14 @@ export default function Page() {
         alert("FETCH QUESTION ERROR:" + err.message);
         console.log("ERROR>>>>>>>>>>>>>>>>>>", err);
       })
-      .finally(() => setLoading(false));
+      .finally(() => {
+        setLoading(false);
+        setShowSpinner(false);
+      });
   };
 
   const takeQuestionHandler = () => {
+    setShowSpinner(true);
     assignQuestion({ doctorId: userData.id, questionId: question.id })
       .then((res) => {
         if (res?.success && res?.statusCode === 200) {
@@ -63,83 +73,106 @@ export default function Page() {
           );
         }
       })
-      .catch((err) => alert(err.message));
+      .catch((err) => alert(err.message))
+      .finally(() => setShowSpinner(false));
   };
 
-  if (loading) return <Loading />;
-
   return (
-    <Fade in={!loading} timeout={700}>
-      <Box>
-        <NavBar />
-        {!question ? (
-          <EmptyQuestion />
-        ) : (
-          <Container maxWidth={false}>
-            <Box sx={{ background: "#fff" }} p={5} mt={3}>
-              <DoubleButton
-                bounty={`${question?.bountyAmount.toLocaleString()} sats ($${(
-                  question?.bountyAmount * USDPerSat
-                ).toLocaleString(undefined, {
-                  maximumFractionDigits: 2,
-                })})`}
-                date={question?.createdAt}
-                cancelBtn={{ text: "Pass", onClick: fetchQuestion }}
-                confirmBtn={{
-                  text: "Take",
-                  onClick: takeQuestionHandler,
-                }}
-              />
-              <Divider />
-              <Box mt={4} mb={4}>
-                <Typography sx={{ fontWeight: "bold" }} mb={3}>
-                  Question
-                </Typography>
-                <Box mb={3}>
-                  <Typography mb={1} color={"#8E8E93"}>
-                    History of your present illness
-                  </Typography>
-                  <Typography whiteSpace={"break-spaces"}>
-                    {question?.content}
-                  </Typography>
-                </Box>
-                {!!question?.currentMedication && (
-                  <Box mb={3}>
-                    <Typography mb={1} color={"#8E8E93"}>
-                      Current medications
-                    </Typography>
-                    <Typography whiteSpace={"break-spaces"}>
-                      {question?.currentMedication}
-                    </Typography>
-                  </Box>
-                )}
-                {!!question?.ageSexEthnicity && (
-                  <Box>
-                    <Typography mb={1} color={"#8E8E93"}>
-                      Age, Sex and Ethnicity
-                    </Typography>
-                    <Typography whiteSpace={"break-spaces"}>
-                      {question?.ageSexEthnicity}
-                    </Typography>
-                  </Box>
-                )}
-              </Box>
-              <Divider />
-              {question.type === "illness" && (
-                <Typography
-                  variant="h5"
-                  textAlign={"center"}
-                  color={"#F68F2A"}
-                  pt={4}
-                >
-                  You can see the full information if you take this question to
-                  leave an answer.
-                </Typography>
-              )}
-            </Box>
-          </Container>
-        )}
-      </Box>
-    </Fade>
+    <Wrapper>
+      <NavBar />
+      {loading && !question ? (
+        <Loading />
+      ) : !question ? (
+        <EmptyQuestion />
+      ) : (
+        <Container>
+          <DoubleButton
+            bounty={`${question?.bountyAmount.toLocaleString()} sats ($${(
+              question?.bountyAmount * USDPerSat
+            ).toLocaleString(undefined, {
+              maximumFractionDigits: 2,
+            })})`}
+            date={question?.createdAt}
+            cancelBtn={{ text: "Pass", onClick: () => fetchQuestion(true) }}
+            confirmBtn={{
+              text: "Take",
+              onClick: takeQuestionHandler,
+            }}
+          />
+          <Divider style={{ marginTop: 32, marginBottom: 32 }} />
+          <Title>Question</Title>
+          <QuestionWrapper>
+            <Subtitle>History of your present illness</Subtitle>
+            <Description>{question?.content}</Description>
+          </QuestionWrapper>
+          {!!question?.currentMedication && (
+            <QuestionWrapper>
+              <Subtitle>Current medications</Subtitle>
+              <Description>{question?.currentMedication}</Description>
+            </QuestionWrapper>
+          )}
+          {!!question?.ageSexEthnicity && (
+            <QuestionWrapper>
+              <Subtitle>Age, Sex and Ethnicity</Subtitle>
+              <Description>{question?.ageSexEthnicity}</Description>
+            </QuestionWrapper>
+          )}
+
+          <Divider style={{ marginTop: 32, marginBottom: 32 }} />
+          {question?.type === "illness" && (
+            <Footnote>
+              You can see the full information if you take this question to
+              leave an answer.
+            </Footnote>
+          )}
+        </Container>
+      )}
+      {showSpinner && <Loading />}
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const Container = styled.div`
+  border-radius: 12px;
+  margin: 40px;
+  padding: 32px;
+  background-color: #fff;
+`;
+
+const QuestionWrapper = styled.div`
+  margin-top: 26px;
+`;
+
+const Title = styled.p`
+  font-size: 17px;
+  font-weight: 600;
+  line-height: 22px;
+  color: #1c1c1e;
+`;
+
+const Subtitle = styled.p`
+  font-size: 13px;
+  font-weight: 600;
+  line-height: 18px;
+  color: #8e8e93;
+  margin-bottom: 8px;
+`;
+
+const Description = styled.p`
+  font-size: 17px;
+  font-weight: 400;
+  line-height: 22px;
+  color: #3a3a3c;
+`;
+
+const Footnote = styled.p`
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 25px;
+  color: #f68f2a;
+  text-align: center;
+`;
