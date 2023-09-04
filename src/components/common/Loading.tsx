612a67db2783768dde1be2c42ec9b64eac05fbbb
Nodirbek Jamoldinov <nick@heartbit.io>
2023-09-04 18:51:03 +0500

    update Loading component

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,16 +1,21 @@
-import { CircularProgress, Container } from "@mui/material";
 import React from "react";
+import { CircularProgress, Container } from "@mui/material";
 
 const Loading = () => {
   return (
     <Container
       maxWidth={false}
       sx={{
+        height: "100vh",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        background: "#fff",
-        height: "100vh",
+        background: "rgba(0,0,0,.2)",
       }}
     >
       <CircularProgress
