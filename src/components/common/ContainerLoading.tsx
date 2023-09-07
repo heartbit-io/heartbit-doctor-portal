2878d8294847e3fcacdf3b0f5e5cf9a1a08e3132
Nodirbek Jamoldinov <nick@heartbit.io>
2023-09-07 23:45:10 +0500

    update ContainerLoading

--- src/components/common/ContainerLoading.tsx
+++ src/components/common/ContainerLoading.tsx
@@ -15,6 +15,9 @@ export default ContainerLoading;
 const LoadingWrapper = styled.div`
   position: absolute;
   top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   width: 100%;
   height: 100%;
   display: flex;
