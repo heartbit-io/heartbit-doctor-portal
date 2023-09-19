c578e659849caa561c8c0370ed1fa2b2427ff082
Nodirbek Jamoldinov <nick@heartbit.io>
2023-09-19 18:44:55 +0500

    change base url

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api: any = axios.create({
-  baseURL: "https://dev-wallet-api.heartbit.io/api/v1/",
+  baseURL: "http://3.39.148.234:8080/api/v1/",
   headers: {
     contentType: "application/json;charset=UTF-8",
   },
